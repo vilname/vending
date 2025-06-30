64fe87b0addc7b64a8c56a81b102cab1fee02153
alexander <tchebaeff.alexander@yandex.ru>
2025-06-30 22:55:54 +0500

    настроил liveReload для фронта

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
       <BrowserRouter>
         <div className="App">
-            <h1>11112</h1>
             <Routes>
                 <Route exact path="/" element={<Home name="Иван" />} />
             </Routes>
